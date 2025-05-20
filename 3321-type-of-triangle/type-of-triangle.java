6a12452ab83129e5a2d62759e57ef3f1a3b4aa0e
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-05-20 18:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (75.16%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -6,7 +6,7 @@ class Solution {
         if(!valid){
             return "none";
         }
-        if (nums[0] == nums[1]  && nums[1] == nums[2]){
+        else if (nums[0] == nums[1]  && nums[1] == nums[2]){
             return "equilateral";
         }
         else if(nums[0] != nums[1]  && nums[1]  != nums[2] && nums[0] != nums[2]){
