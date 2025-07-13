19aa44ba990a8110c41d21073cb1e0b89d4d6cbc
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-07-13 15:43:05 +0530

    Time: 3 ms (100.00%) | Memory: 53.8 MB (95.73%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String reverse(String sb){
+        int start = 0;
+        int end = sb.length() -1;
+        char arr[] = sb.toCharArray();
+        while(start < end){
+            char ch = arr[start];
+            arr[start]= arr[end];
+            arr[end] = ch;
+            start++;
+            end--;
+        }
+        return String.valueOf(arr);
+    }
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder("");
+
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch == '*' ){
+                if(res.length() > 0){
+                    res.deleteCharAt(res.length() -1);
+                }
+            }
+            else if(ch == '#'){
+                res.append(res);
+            }
+            else if(ch == '%'){
+                res = new StringBuilder(reverse(res.toString()));
+            }
+
+            else {
+                res.append(ch);
+            }
+        }
+        return res.toString();
+    }
+}
