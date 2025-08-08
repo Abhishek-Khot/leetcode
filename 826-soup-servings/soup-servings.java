c6c16175340c1fab684365411e1e4ab1870a67c7
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-08-08 09:19:14 +0530

    Time: 53 ms (17.37%) | Memory: 230.2 MB (14.97%) - LeetSync

---
+++ 826-soup-servings/soup-servings.java
@@ -0,0 +1,40 @@
+class Solution {
+    //two variables means two dimension dp 
+    double[][] dp;
+    int serves[][] = new int[][]{{100,0},{75,25},{50,50},{25,75}};
+    public double solve(int A,int B){
+        //all this is from the question
+        if(A <= 0 && B <= 0){
+            return 0.5;
+        }
+        if(A <= 0){
+            return 1.0;
+        }
+        if(B <= 0){
+            return 0.0;
+        }
+        if(dp[A][B] != -1.0){
+            return dp[A][B];
+        }
+        double prob = 0.0;
+        for(int serve[] : serves){
+            int choose_A = serve[0];
+            int choose_B = serve[1];
+            prob += solve(A - choose_A , B - choose_B);
+        }
+        return dp[A][B] = 0.25 * prob;
+    }
+    public double soupServings(int n) {
+        //to avoid the large values at last
+        if(n >= 5000){
+            return 1.0;
+        }
+        
+        dp = new double[n+1][n+1];
+        
+        for(double d[] : dp){
+            Arrays.fill(d,-1.0);
+        }
+        return solve(n,n);
+    }
+}
