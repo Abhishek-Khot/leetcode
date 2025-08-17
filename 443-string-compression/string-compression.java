f0977fc6bcd2799e26d474b81ffee24ec014498d
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-08-17 19:42:26 +0530

    Time: 1 ms (99.64%) | Memory: 44 MB (79.66%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,28 +1,26 @@
 class Solution {
-    //two pointer i and index just dry run 
     public int compress(char[] chars) {
         int n = chars.length;
         int i = 0;
-        int index = 0;
-        while (i < n) {
-            char curr_char = chars[i];
+        int idx = 0;
+        while(i < n){
+            char curr = chars[i];
             int count = 0;
-            while (i < n && curr_char == chars[i]) {
-                i++;
+            while( i < n && curr == chars[i]){
                 count++;
+                i++;
             }
-            chars[index] = curr_char;
-            index++;
-            //if count is two digit 
-            String str = String.valueOf(count);
-            //if count is 1 then no need to do that 
-            if (count > 1) {
-                for (char ch : str.toCharArray()) {
-                    chars[index] = ch;
-                    index++;
+            chars[idx] = curr;
+            idx++;
+
+            String co = String.valueOf(count);
+            if(count > 1){
+                for(char ch : co.toCharArray()){
+                    chars[idx] = ch;
+                    idx++;
                 }
             }
         }
-        return index;
+        return idx;
     }
 }
