4573487cd449d6c71bb3144b2ade6c279b1b2cea
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-11-04 12:14:17 +0530

    Time: 2 ms (85.60%) | Memory: 45.9 MB (5.47%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,25 @@
 class Solution {
-    public void findComb(int[] candidates,int target,List<List<Integer>> res,List<Integer> local,int idx){
-        if(idx == candidates.length){
-            if(target == 0){
+    public void findSubsets(int[] candidates, int target, List<List<Integer>> res, List<Integer> local, int idx) {
+        if (idx == candidates.length) {
+            if (target == 0) {
                 res.add(new ArrayList<>(local));
                 return;
             }
             return;
         }
-        if(candidates[idx] <= target){
+        if (candidates[idx] <= target) {
             local.add(candidates[idx]);
-            findComb(candidates,target - candidates[idx],res,local,idx);
-            local.remove(local.size()-1);
+            findSubsets(candidates, target - candidates[idx], res, local, idx);
+            local.remove(local.size() - 1);
         }
-        findComb(candidates,target,res,local,idx+1);
+        findSubsets(candidates, target, res, local, idx + 1);
     }
+
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> local = new ArrayList<>();
-
-        findComb(candidates,target,res,local,0);
+        int idx = 0;
+        findSubsets(candidates, target, res, local, idx);
         return res;
     }
 }
