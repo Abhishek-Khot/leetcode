a8d81f84593cfdd32d448c0c18ebd74e417e7131
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-11-04 07:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (5.09%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -5,6 +5,7 @@ class Solution {
 
         for(int i =0;i<n;i++){
             while(!strs[i].startsWith(pref)){
+                //0 to n -1 will be the indexing so use this will remove last character every time 
                 pref = pref.substring(0,pref.length()-1);
             }
         }
