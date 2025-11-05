482d94f795e5bdf6b942aa5873c5c2c510612af1
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-11-05 07:52:01 +0530

    Time: 1 ms (58.26%) | Memory: 48.9 MB (6.54%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int[] findLeftHt(int[] height){
+    public static int[] findL(int height[]){
         int n = height.length;
         int left[] = new int[n];
-        left[0]  = height[0];
+        left[0] = height[0];
         for(int i = 1;i<n;i++){
-            left[i]  = Math.max(left[i-1],height[i]);
+            left[i] = Math.max(left[i-1],height[i]);
         }
         return left;
     }
-    public int[] findRightHt(int[] height){
+    public static int[] findR(int height[]){
         int n = height.length;
         int right[] = new int[n];
-        right[n-1]  = height[n-1];
+        right[n-1] = height[n-1];
         for(int i = n-2;i>=0;i--){
-            right[i]  = Math.max(right[i+1],height[i]);
+            right[i] = Math.max(right[i+1],height[i]);
         }
         return right;
     }
     public int trap(int[] height) {
-        int left[] = findLeftHt(height);
-        int right[] = findRightHt(height);
+        int left[] = findL(height);
+        int right[] = findR(height);
         int sum = 0;
         int n = height.length;
         for(int i = 0;i<n;i++){
             int water = Math.min(left[i],right[i]);
-            sum+= (water - height[i]);
+            sum+=(water-height[i]);
         }
         return sum;
     }
