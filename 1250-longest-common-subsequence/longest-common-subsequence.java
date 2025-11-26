ff78fe9e0ee4bca3867765dfca5fca57b9940f36
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-11-26 16:52:37 +0530

    Time: 60 ms (5.10%) | Memory: 109.3 MB (5.05%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,16 +1,6 @@
 class Solution {
     // abcde eca 
     public int dp[][] = new int[1001][1001];
-    public String reverse(String s){
-        char arr[] = s.toCharArray();
-        int n = arr.length;
-        for(int i = 0;i<n/2;i++){
-            char ch = arr[i];
-            arr[i] = arr[n-i-1];
-            arr[n-i-1] = ch;
-        }
-        return String.valueOf(arr);
-    }
     public int solve(String txt1,String txt2,int i,int j){
         if(i == txt1.length() || j == txt2.length()){
             return 0;
@@ -28,7 +18,6 @@ class Solution {
         for(int d[] : dp){
             Arrays.fill(d,-1);
         }
-        // String rev = reverse(text2);
         return solve(text1,text2,0,0);
     }
 }
