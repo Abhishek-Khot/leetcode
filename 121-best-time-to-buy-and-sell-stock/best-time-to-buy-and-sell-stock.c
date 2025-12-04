2b205ec4db6cdc8147f6df8f87ecfb8ccd870ae7
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2025-12-05 00:34:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (17.76%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -1,17 +1,15 @@
+int min(int a,int b){
+    return a > b ? b : a;
+}
 int max(int a,int b){
-    return a > b ? a : b;
+    return a > b ? a: b;
 }
 int maxProfit(int* prices, int pricesSize) {
     int maxProfit = 0;
-    int buyPrice = prices[0]; 
-    for(int i = 1;i<pricesSize;i++){
-        if(buyPrice < prices[i]){
-            int profit = prices[i] - buyPrice;
-            maxProfit = max(maxProfit,profit);
-        }
-        else{
-            buyPrice = prices[i];
-        }
+    int minPrice = prices[0];
+    for(int i= 0;i<pricesSize;i++){
+        minPrice = min(minPrice,prices[i]);
+        maxProfit = max(maxProfit,prices[i] - minPrice);
     }
     return maxProfit;
 }
