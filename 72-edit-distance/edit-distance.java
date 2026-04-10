03d17f64942d60b9cfeff3cebb3b50bce2421c3c
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2026-04-10 19:54:50 +0530

    Time: 3 ms (98.38%) | Memory: 47.2 MB (40.68%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -14,7 +14,7 @@ class Solution {
         }
         // both chars are same just move to next no need to anything
         if(word1.charAt(i) == word2.charAt(j)){
-            return solve(word1,word2,i+1,j+1);
+            return dp[i][j]= solve(word1,word2,i+1,j+1);
         }
         // if both are different try each operation -> take minimum
         else{
