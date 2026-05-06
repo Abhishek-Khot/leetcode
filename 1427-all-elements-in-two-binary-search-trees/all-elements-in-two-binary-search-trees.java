177430f0ed72bc32a5222c397d5d50be7427cc50
Abhishek Khot <128317286+Abhishek-Khot@users.noreply.github.com>
2026-05-06 10:53:22 +0530

    Time: 17 ms (84.64%) | Memory: 48.1 MB (99.14%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+
+    public List<Integer>  merge(List<Integer> list1,List<Integer> list2){
+        List<Integer> list3 = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i < list1.size() && j < list2.size()){
+            if(list1.get(i) < list2.get(j)){
+                list3.add(list1.get(i));
+                i++;
+            }
+            else{
+                list3.add(list2.get(j));
+                j++;
+            }
+        }
+        while(i< list1.size()){
+            list3.add(list1.get(i));
+            i++;
+        }
+        while(j < list2.size()){
+            list3.add(list2.get(j));
+            j++;
+        }
+        return list3;
+    }
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        inOrder(root1, list1);
+        inOrder(root2, list2);
+
+        return merge(list1, list2);
+        
+    }
+}
